4eb0e678642cfa7d898d65bfd108c01f3e2b2349
Josh Andromidas <git@joshandromidas.com>
2022-04-04 15:30:24 -0500

    clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-// Import containers and components
-import { detailStore, ratingStore } from './stores.js';
 import ProductDetail from './containers/ProductDetail.jsx';
 import './App.scss';
 
 function App() {
-  const currentCount = detailStore((state) => state.counter + 1);
-  const goUpBoi = detailStore((state) => state.increaseCounter);
-  const goDownBoi = detailStore((state) => state.decreaseCounter);
-  const dogs = ratingStore((state) => state.dogs);
-  const decreaseDogs = ratingStore((state) => state.decreaseDogs);
-
   return (
-    <>
-      <h1>Hello World</h1>
-      <h2>{`Count: ${currentCount}`}</h2>
-      <button type="submit" onClick={goDownBoi}>go DOWN boi</button>
-      <button type="submit" onClick={goUpBoi}>go UP boi</button>
-      <h2>{`Dogs: ${dogs}`}</h2>
-      <button type="submit" onClick={decreaseDogs}>- dogs</button>
-      <button type="submit" onClick={ratingStore((state) => state.increaseDogs)}>+ dogs</button>
+    <div>
       <ProductDetail />
-    </>
+    </div>
   );
 }
 
