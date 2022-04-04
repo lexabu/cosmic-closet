da346a5fc33823888d2f8496c04c71df1b68a698
Josh Andromidas <git@joshandromidas.com>
2022-04-04 12:00:48 -0500

    relocate test files to leave room for initial PD page setup

--- src/containers/ProductDetail.jsx
+++ src/containers/ProductDetail.jsx
@@ -1,16 +1,11 @@
 // Test for zustand!
 import React from 'react';
-import { detailStore } from '../stores.js';
-import Nested from './nested.jsx';
 
 function ProductDetail() {
-  const count = detailStore((state) => state.counter);
-
   return (
     <>
       <h1>ProductDetail</h1>
-      <h2>{`Count: ${count}`}</h2>
-      <Nested />
+      <div>asdf</div>
     </>
   );
 }
