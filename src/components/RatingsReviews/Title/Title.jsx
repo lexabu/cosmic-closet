1f6f443967fda5ee41b60ad37949a70264646550
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-04-04 15:39:10 -0500

    scaffolding for ratings and reviews

---
+++ src/components/RatingsReviews/Title/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <div className="rr-Title"> RATINGS & REVIEWS NEW NEW NEW</div>
+  );
+}
+
+export default Title;
---
+++ src/components/RatingsReviews/index.js
@@ -0,0 +1,3 @@
+export { default as StarRating } from './StarRating/StarRating.jsx';
+export { default as StarBreakdown } from './StarBreakdown/StarBreakdown.jsx';
+export { default as Title } from './Title/Title.jsx';
