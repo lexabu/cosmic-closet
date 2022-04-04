873bf2a2ae3f36246835a838c856e4ddd528e29c
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-04-04 15:49:55 -0500

    fix related items

--- src/containers/RelatedItems.jsx
+++ src/containers/RelatedItems.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-// Import containers and components
-import { detailStore, ratingStore } from './stores.js';
-import ProductDetail from './containers/ProductDetail.jsx';
 
-function App() {
-  const currentCount = detailStore((state) => state.counter);
-  const goUpBoi = detailStore((state) => state.increaseCounter);
-  const goDownBoi = detailStore((state) => state.decreaseCounter);
-  const dogs = ratingStore((state) => state.dogs);
-  const decreaseDogs = ratingStore((state) => state.decreaseDogs);
-
+function RelatedItems() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <h2>{`Count: ${currentCount}`}</h2>
-      <button type="submit" onClick={goDownBoi}>go DOWN boi</button>
-      <button type="submit" onClick={goUpBoi}>go UP boi</button>
-      <h2>{`Dogs: ${dogs}`}</h2>
-      <button type="submit" onClick={decreaseDogs}>- dogs</button>
-      <button type="submit" onClick={ratingStore((state) => state.increaseDogs)}>+ dogs</button>
-      <ProductDetail />
-    </>
+    <div>
+      <div> Related Items</div>
+    </div>
   );
 }
 
-export default App;
+export default RelatedItems;
