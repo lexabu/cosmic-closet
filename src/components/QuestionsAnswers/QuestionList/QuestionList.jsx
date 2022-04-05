f9c61fcc45e9d0d411489846f1d00fafaecf627c
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-04-05 14:25:01 -0500

    adds mapQuestions function to QuestionsList.jsx

--- src/components/QuestionsAnswers/QuestionList/QuestionList.jsx
+++ src/components/QuestionsAnswers/QuestionList/QuestionList.jsx
@@ -30,51 +30,31 @@ function QuestionList() {
 
   console.log(allQuestions);
 
+  function mapQuestions(questionsArr) {
+    return questionsArr.map((question) => (
+      <div key={question.question_id}>
+        <div>{`Q: ${question.question_body}`}</div>
+        <div>
+          Helpful?
+          <a href="Yes">Yes</a>
+          (0)|
+          <a href="#Add Answer">Add Answer</a>
+        </div>
+      </div>
+    ));
+  }
+
   return (
     <div>
       <div>
-        {/* <div>
-          {allQuestions.map((question) => (<div key={question.question_id}>{question}</div>))}
-        </div> */}
         <div className="title"> Questions & Answers</div>
         <SearchBar />
-        Q: question1
-        <div>Helpful? Yes(0) | Add Answer</div>
+        <div>{mapQuestions(allQuestions)}</div>
         <div>
           A: Answer1
           <div>by user1, May 14, 2022</div>
           Answer2
           <div>by user1, May 15, 2022</div>
-        </div>
-      </div>
-      <div>
-        Q: question2
-        <div>Helpful? Yes(0) | Add Answer</div>
-        <div>
-          A: Answer1
-          <div>by user1, May 16, 2022</div>
-          Answer2
-          <div>by user1, May 17, 2022</div>
-        </div>
-      </div>
-      <div>
-        Q: question3
-        <div>Helpful? Yes(0) | Add Answer</div>
-        <div>
-          A: Answer1
-          <div>by user1, May 18, 2022</div>
-          Answer2
-          <div>by user1, May 19, 2022</div>
-        </div>
-      </div>
-      <div>
-        Q: question4
-        <div>Helpful? Yes(0) | Add Answer</div>
-        <div>
-          A: Answer1
-          <div>by user1, May 20, 2022</div>
-          Answer2
-          <div>by user1, May 21, 2022</div>
         </div>
       </div>
       <button type="button">More Answered Questions</button>
