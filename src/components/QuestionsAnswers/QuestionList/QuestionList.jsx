5318360ed440e265cea2290ca3763102da97e8c3
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-04-05 14:38:25 -0500

    adds helpful comments and removes extra comments

--- src/components/QuestionsAnswers/QuestionList/QuestionList.jsx
+++ src/components/QuestionsAnswers/QuestionList/QuestionList.jsx
@@ -3,14 +3,13 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { questionsStore } from '../../../stores.js';
 import SearchBar from '../SearchBar/SearchBar.jsx';
-// import dummyQuestions from './dummyQuestions.js';
-// import { useParams} from 'react-router-dom';
 
 function QuestionList() {
   const setQuestions = questionsStore((state) => state.setQuestions);
   const allQuestions = questionsStore((state) => state.questions);
   const { id } = useParams();
 
+  // API call to access all questions associated with the current product
   useEffect(() => {
     axios({
       url: `${process.env.URL}qa/questions?product_id=${id}`,
@@ -20,7 +19,6 @@ function QuestionList() {
       },
     })
       .then((data) => {
-        console.log('data :', data);
         setQuestions(data.data.results);
       })
       .catch((err) => {
@@ -28,15 +26,13 @@ function QuestionList() {
       });
   }, []);
 
-  console.log(allQuestions);
-
   function mapQuestions(questionsArr) {
     return questionsArr.map((question) => (
       <div key={question.question_id}>
         <div>{`Q: ${question.question_body}`}</div>
         <div>
           Helpful?
-          <a href="Yes">Yes</a>
+          <a href="#Yes">Yes</a>
           (0)|
           <a href="#Add Answer">Add Answer</a>
         </div>
@@ -50,6 +46,8 @@ function QuestionList() {
         <div className="title"> Questions & Answers</div>
         <SearchBar />
         <div>{mapQuestions(allQuestions)}</div>
+        {/* The following div is hard-coded. It will be replaced
+        once my Answer comp is up and running */}
         <div>
           A: Answer1
           <div>by user1, May 14, 2022</div>
