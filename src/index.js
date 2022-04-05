b709edad965d91ba76a4c8361ab33c561cd85c5d
Josh Andromidas <git@joshandromidas.com>
2022-04-05 09:56:24 -0500

    add comments and notes

--- src/index.js
+++ src/index.js
@@ -5,26 +5,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { createRoot } from 'react-dom/client'; // new way for React 18
 // import reactDOM from "react-dom"; //old way for React 17
+
 import './index.scss';
 import App from './App.jsx';
-
-// TODO
-// our scss import statement
-// import "./index.scss";
-
-// TODO AIRBNB linter
 
 // render the App
 const container = document.getElementById('root');
 // createRoot is a new method for React 18 that is imported in the import statements
 const root = createRoot(container);
 root.render(
+  // BrowserRouter is a tool that lets you specify a component to be shown
+  // based on the URL path. We only have one page showing our App component,
+  // but we have multiple products. The :id is basically a wildcard that will
+  // give the name 'id' to whatever is in that spot. Now we can have multiple
+  // URLs referring to different products, while all coming from the same components.
   <BrowserRouter>
-    {/* <App /> */}
     <Routes>
+      {/* /:id means any path with something after the '/' will be the id.
+                     i.e. :   localhost:3000/123 -> id === 123
+          or when deployed:         site.com/123 -> id === 123
+      */}
       <Route path="/:id" element={<App />} />
     </Routes>
   </BrowserRouter>,
 );
+
 // createRoot(document.getElementById('root')).render(<App />) // new way
 // reactDOM.render(<App />, document.getElementById("root")); // old way
