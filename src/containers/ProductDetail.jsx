09c3692a9ac62abacf1578c9599ae100da4be088
Josh Andromidas <git@joshandromidas.com>
2022-04-04 22:59:34 -0500

    working api call to get title, category, and price

--- src/containers/ProductDetail.jsx
+++ src/containers/ProductDetail.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { detailStore } from '../stores.js';
 import { RightDetails, ImageGallery, DetailsFooter } from '../components/ProductDetail';
 
 function ProductDetail() {
   const { id } = useParams();
+
+  const setApiResults = detailStore((state) => state.setDetailsApiResults);
+  const res = detailStore((state) => state.detailsApiResults);
+
+  console.log('res', res);
+
+  useEffect(() => {
+    console.log('useEffect() running...');
+    axios.get(`${process.env.URL}products/${id}`, {
+      headers: {
+        Authorization: process.env.GITHUB_API_KEY,
+      },
+    })
+      .then((results) => {
+        setApiResults(results.data);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
+  // console.log(process.env.URL);
+
   return (
     <>
       <h1 className="temp-title">{id}</h1>
--- src/stores.js
+++ src/stores.js
@@ -4,13 +4,11 @@ import { devtools } from 'zustand/middleware';
 const detailStore = create(
   // All this does is include middleware to add the store to the Redux chrome extension
   devtools((set) => ({
-    counter: 0,
-    increaseCounter: () => set((state) => ({ counter: state.counter + 1 })),
-    decreaseCounter: () => set((state) => ({ counter: state.counter - 1 })),
-    apiResults: [],
-    setApiResults: (data) => set(() => ({ apiResults: data })),
-    filteredApiResults: [],
-    setFilteredApiResults: (data) => set(() => ({ filteredApiResults: data })),
+    // counter: 0,
+    // increaseCounter: () => set((state) => ({ counter: state.counter + 1 })),
+    // decreaseCounter: () => set((state) => ({ counter: state.counter - 1 })),
+    detailsApiResults: [],
+    setDetailsApiResults: (data) => set({ detailsApiResults: data }),
   })),
 );
 
