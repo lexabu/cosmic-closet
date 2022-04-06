a06b20d4c5e946cb9c7d4c136c149eefd9df7c5d
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-04-06 18:27:42 -0500

    percentOfReviewsRecommendProduct and progress bar

--- src/components/RatingsReviews/RatingBreakdown/RatingBreakdown.jsx
+++ src/components/RatingsReviews/RatingBreakdown/RatingBreakdown.jsx
@@ -1,10 +1,70 @@
 import React from 'react';
 import './RatingBreakdown.scss';
+import { reviewMetaStore, reviewStore } from '../../../stores.js';
 
 function RatingBreakdown() {
-  return (
-    <div className="rr-RatingBreakdown">RatingBreakdown</div>
-  );
+  const metaRatings = reviewMetaStore((state) => state.ratings);
+  const { ratings, recommended } = metaRatings;
+  console.log('recommended: ', recommended);
+  console.log('ratings: ', ratings);
+
+  // need a numerator to be the number of true
+  // need the denominator to be the count of true and false
+
+  if (recommended) {
+    // percent reccommended
+    const recommendedTrue = Number(recommended.true);
+    const recommendedFalse = Number(recommended.false);
+    const numerator = recommendedTrue;
+    const denominator = recommendedTrue + recommendedFalse;
+    const percentOfReviewsRecommendProduct = ((numerator / denominator) * 100).toFixed(0);
+
+    // breakdown
+    const one = ratings['1'] || 0;
+    const two = ratings['2'] || 0;
+    const three = ratings['3'] || 0;
+    const four = ratings['4'] || 0;
+    const five = ratings['5'] || 0;
+
+    // const [one] = Object.values(ratings);
+
+    console.log('one: ', one);
+
+    return (
+      <div className="rr-rating-breakdown">
+
+        <div className="rr-rb-header">
+          {percentOfReviewsRecommendProduct}
+          % of reviews recommend this product
+        </div>
+
+        <div className="rr-rb-body">
+          <div className="rr-rb-body-item" id="five">
+            5 stars &nbsp;
+            <progress max={denominator} value={five} />
+          </div>
+          <div className="rr-rb-body-item" id="four">
+            4 stars &nbsp;
+            <progress max={denominator} value={four} />
+          </div>
+          <div className="rr-rb-body-item" id="three">
+            3 stars &nbsp;
+            <progress max={denominator} value={three} />
+          </div>
+          <div className="rr-rb-body-item" id="two">
+            2 stars &nbsp;
+            <progress max={denominator} value={two} />
+          </div>
+          <div className="rr-rb-body-item" id="one">
+            1 stars &nbsp;
+            <progress max={denominator} value={one} />
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+  return <div> Loading... </div>;
 }
 
 export default RatingBreakdown;
--- src/components/RatingsReviews/Star/Star.jsx
+++ src/components/RatingsReviews/Star/Star.jsx
@@ -3,14 +3,11 @@ import { FaStar } from 'react-icons/fa';
 import './Star.scss';
 
 function Star({ filled }) {
-  console.log('filled: ', filled);
   return (
-
     <FaStar
       className="star"
       color={filled ? 'blue' : 'lightgray'}
     />
-
   );
 }
 export default Star;
