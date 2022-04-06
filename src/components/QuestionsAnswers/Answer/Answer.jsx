6ddfdcff4f6f7c6a82d7dd571608bb8ef19e302b
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-04-06 10:01:29 -0500

    adds Answers component

---
+++ src/components/QuestionsAnswers/Answer/Answer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Answer() {
+  return (
+    <div> Answer Comp </div>
+  );
+}
+
+export default Answer;
--- src/components/QuestionsAnswers/index.js
+++ src/components/QuestionsAnswers/index.js
@@ -1,3 +1,4 @@
 export { default as Question } from './Question/Question.jsx';
 export { default as QuestionList } from './QuestionList/QuestionList.jsx';
+export { default as Answer } from './Answer/Answer.jsx';
 export { default as SearchBar } from './SearchBar/SearchBar.jsx';
