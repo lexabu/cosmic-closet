61a25bf855dff2791bc3c515a56c09e87432b37f
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-04-07 15:23:05 -0500

    date formatted for answers

--- src/components/QuestionsAnswers/Answer/Answer.jsx
+++ src/components/QuestionsAnswers/Answer/Answer.jsx
@@ -30,6 +30,15 @@ function Answer({ questionObj }) {
 
   console.log('after call ans :', allAnswers);
 
+  // function dateFormatter(apiDate) {
+  //   const dateString = apiDate.slice(0, 10);
+  //   const longDate = new Date(dateString);
+  //   console.log('date type :', typeof longDate);
+  //   console.log(Object.values(longDate));
+  //   // const date = longDate.slice(4);
+  //   return longDate;
+  // }
+
   function mapAnswers(answersObj) {
     console.log('answersObj :', answersObj);
     console.log('answersObjValues :', Object.values(answersObj));
@@ -37,7 +46,7 @@ function Answer({ questionObj }) {
       // console.log('answer :', answer)
       <div key={answer.id}>
         <div>{`A: ${answer.body}`}</div>
-        <div>{`by ${answer.answerer_name}, ${answer.date} | Helpful? Yes(${answer.helpfulness}) | Report`}</div>
+        <div>{`by ${answer.answerer_name} ${answer.date.slice(0, 10)} | Helpful? Yes(${answer.helpfulness}) | Report`}</div>
       </div>
     ));
   }
