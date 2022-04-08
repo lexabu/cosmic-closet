060a43624cb174278620753e199dc4a548676780
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-04-07 22:03:36 -0500

    allows for helpfulness to update in questions component

--- src/components/QuestionsAnswers/Question/Question.jsx
+++ src/components/QuestionsAnswers/Question/Question.jsx
@@ -1,14 +1,45 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/prop-types */
 import React from 'react';
-// import Answer from '../index.js';
-import Answer from '../Answer/Answer.jsx';
+import axios from 'axios';
+import { Answer } from '../index.js';
 
-function Question({ questionObj }) {
+function Question({ questionObj, handleQuestions }) {
+  function getUpdateHelpfulness(question) {
+    axios({
+      url: `${process.env.URL}qa/questions/${question.question_id}/helpful`,
+      method: 'PUT',
+      headers: {
+        Authorization: process.env.GITHUB_API_KEY,
+      },
+    })
+      .then(() => {
+        handleQuestions();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
+  // function handleKeyPress(event, question) {
+  //   if (event.key === 'Enter') {
+  //     getUpdateHelpfulness(question);
+  //   }
+  // }
+
   return (
     <div>
       <div>{`Q: ${questionObj.question_body}`}</div>
-      <div>Helpful? Yes({questionObj.question_helpfulness}) | Add Answer</div>
+      <div>Helpful? | </div>
+      <div
+        role="button"
+        tabIndex={0}
+        // onKeyPress={() => (handleKeyPress(questionObj))}
+        onClick={() => (getUpdateHelpfulness(questionObj))}
+      >
+        Yes({questionObj.question_helpfulness})
+      </div>
+      <div> | Add Answer</div>
       <Answer questionObj={questionObj} />
     </div>
   );
