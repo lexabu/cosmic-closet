6fbea1022e8c4338aa4a555b2e8384cf178ba003
Josh Andromidas <git@joshandromidas.com>
2022-04-11 18:07:50 -0500

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
 import ProductDetail from './containers/ProductDetail.jsx';
 import QuestionsAnswers from './containers/QuestionsAnswers.jsx';
 import RatingsReviews from './containers/RatingsReviews.jsx';
+import { detailStore } from './stores.js';
 
 function App() {
+  const handleAnalytics = detailStore((state) => state.handleAnalytics);
+
+  useEffect(() => {
+    window.addEventListener('click', handleAnalytics);
+  }, []);
+
   return (
     <main>
       <ProductDetail />
