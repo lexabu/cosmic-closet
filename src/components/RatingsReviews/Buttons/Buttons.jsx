63e7a8f0c545afdf280bcf539f321c3ab74cc356
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-04-11 16:27:57 -0500

    update buttons to remove action-button class

--- src/components/RatingsReviews/Buttons/Buttons.jsx
+++ src/components/RatingsReviews/Buttons/Buttons.jsx
@@ -4,8 +4,8 @@ import './Buttons.scss';
 function Buttons() {
   return (
     <div className="rr-buttons">
-      <button type="button" className="rr-buttons-more-reviews action-button"> More Reviews </button>
-      <button type="button" className="rr-buttons-add-review action-button"> Add a Review + </button>
+      <button type="button" className="rr-buttons-more-reviews"> More Reviews </button>
+      <button type="button" className="rr-buttons-add-review"> Add a Review + </button>
     </div>
   );
 }
