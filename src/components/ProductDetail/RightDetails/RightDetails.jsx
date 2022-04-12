433af701ea3aacb1fe54a471e95d707f0450d382
Josh Andromidas <git@joshandromidas.com>
2022-04-12 12:19:15 -0500

    add components for ratings

--- src/components/ProductDetail/RightDetails/RightDetails.jsx
+++ src/components/ProductDetail/RightDetails/RightDetails.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { detailStore } from '../../../stores.js';
+import { detailStore, reviewStore } from '../../../stores.js';
 import Styles from '../Styles/Styles.jsx';
 import CartActions from '../CartActions/CartActions.jsx';
 import './RightDetails.scss';
+import { StarRating } from '../../RatingsReviews/index.js';
 
 function RightDetails() {
-  const reviewsCount = 209;
   const title = detailStore((state) => state.productDetails.name);
   const category = detailStore((state) => state.productDetails.category);
   const selectedStyle = detailStore((state) => state.selectedStyle);
   const imageZoomed = detailStore((state) => state.imageZoomed);
+  const averageRating = reviewStore((state) => state.averageRating);
+  const reviews = reviewStore((state) => state.reviews);
+  const reviewsCount = reviews.results ? reviews.results.length : 'uhhh';
+
   const originalPrice = selectedStyle.original_price;
   const salePrice = selectedStyle.sale_price;
+
   let onSale = false;
 
   if (salePrice) {
@@ -21,7 +26,7 @@ function RightDetails() {
   return (
     <div className={`pd-right-details-container${imageZoomed ? ' hidden' : ''}`}>
       <div className="pd-stars-container">
-        <div>⭐⭐⭐⭐⭐</div>
+        <StarRating rating={averageRating} />
         <span className="pd-read-all-reviews">{`Read all ${reviewsCount} reviews`}</span>
       </div>
       <div className="pd-info">
