04175fa930263d86b946601a9fcc46acdd4075ec
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-04-12 12:24:17 -0500

    finalize URL clean up

--- src/containers/RatingsReviews.jsx
+++ src/containers/RatingsReviews.jsx
@@ -6,26 +6,6 @@ import { LeftColumn, RightColumn } from '../components/RatingsReviews/index.js';
 
 function RatingsReviews() {
   const { id } = useParams();
-
-  const ratingsURL = new URL(`${process.env.URL}reviews/meta`);
-  const setRatings = reviewStore((state) => state.setRatings);
-
-  // todo: refactor to use reviewsURL for new URL(`${process.env.URL}reviews`)
-
-  const reviewsURL = new URL(`${process.env.URL}reviews`);
-  const helpfulReviewsURL = reviewsURL;
-  helpfulReviewsURL.searchParams.set('sort', 'helpful');
-  const setHelpfulReviews = reviewStore((state) => state.setHelpfulReviews);
-
-  const newestReviewsURL = reviewsURL;
-  newestReviewsURL.searchParams.set('sort', 'newest');
-  const setNewestReviews = reviewStore((state) => state.setNewestReviews);
-
-  const relevantReviewsURL = reviewsURL;
-  relevantReviewsURL.searchParams.set('sort', 'relevant');
-  const setRelevantReviews = reviewStore((state) => state.setRelevantReviews);
-  const setReviews = reviewStore((state) => state.setReviews);
-
   const authHeaders = {
     headers: {
       Authorization: process.env.GITHUB_API_KEY,
@@ -36,13 +16,25 @@ function RatingsReviews() {
     },
   };
 
+  const ratingsURL = new URL(`${process.env.URL}reviews/meta`);
+  const { helpfulReviewsURL, newestReviewsURL, relevantReviewsURL } = new URL(`${process.env.URL}reviews`);
+
+  helpfulReviewsURL.searchParams.set('sort', 'helpful');
+  newestReviewsURL.searchParams.set('sort', 'newest');
+  relevantReviewsURL.searchParams.set('sort', 'relevant');
+
+  const setHelpfulReviews = reviewStore((state) => state.setHelpfulReviews);
+  const setNewestReviews = reviewStore((state) => state.setNewestReviews);
+  const setRatings = reviewStore((state) => state.setRatings);
+  const setRelevantReviews = reviewStore((state) => state.setRelevantReviews);
+  const setReviews = reviewStore((state) => state.setReviews);
+
   useEffect(() => {
     axios.get(ratingsURL.toString(), authHeaders)
       .then((results) => {
         setRatings(results.data);
       })
       .catch((err) => {
-        console.log('ratingsURL error: ', err);
         throw err;
       });
   }, []);
@@ -53,7 +45,6 @@ function RatingsReviews() {
         setHelpfulReviews(results.data);
       })
       .catch((err) => {
-        console.log('helpfulReviewsURL error: ', err);
         throw err;
       });
   }, []);
@@ -64,7 +55,6 @@ function RatingsReviews() {
         setNewestReviews(results.data);
       })
       .catch((err) => {
-        console.log('newestReviewsURL error: ', err);
         throw err;
       });
   }, []);
@@ -76,7 +66,6 @@ function RatingsReviews() {
         setReviews(results.data);
       })
       .catch((err) => {
-        console.log('relevantReviewsURL error: ', err);
         throw err;
       });
   }, []);
