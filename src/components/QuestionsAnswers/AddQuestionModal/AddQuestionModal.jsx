35051bbc98bec5630a07ef70fe7ef21ddbe2ee0c
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-04-12 16:58:21 -0500

    adds style to the questions modal

--- src/components/QuestionsAnswers/AddQuestionModal/AddQuestionModal.jsx
+++ src/components/QuestionsAnswers/AddQuestionModal/AddQuestionModal.jsx
@@ -1,42 +1,43 @@
 import React, { useState } from 'react';
 
-
 import './AddQuestionModal.scss';
 
 function AddQuestionModal() {
   const [openQuestionModal, setOpenQuestionModal] = useState(false);
-
-  return (
-    <div className="qa-question-modal">
-      <div className="qa-question-modal-content">
-        <button className="qa-question-modal-close small-pill" type="button" onClick={() => setOpenQuestionModal(true)}> X </button>
-        <div className="qa-question-modal-header">
-          <h1 className="qa-question-modal-title">Ask Your Question</h1>
+  if (!openQuestionModal) {
+    return (
+      <div className="qa-question-modal">
+        <div className="qa-question-modal-content">
+          <button className="qa-question-modal-close small-pill" type="button" onClick={() => setOpenQuestionModal(true)}> X </button>
+          <div className="qa-question-modal-header">
+            <h2 className="qa-question-modal-title">Ask Your Question</h2>
+          </div>
+          <div className="qa-question-modal-sub-header">
+            <h3 className="qa-question-modal-title">About the Product Name Here</h3>
+          </div>
+          {/* onSubmit={this.handleSubmit} */}
+          <form className="qa-question-modal-form">
+            <label htmlFor="question" className="qa-question-modal-label">
+              Your Question*:
+              <textarea id="question" rows="4" cols="50" maxLength={1000} placeholder="Why did you like the product or not?" />
+              {/* <input type="text" id="question" /> */}
+            </label>
+            <label htmlFor="nickname" className="qa-question-modal-label">
+              Nickname*:
+              <input type="text" id="nickname" maxLength={60} placeholder="jackson11!" />
+              <h5>For privacy reasons, do not use your full name or email address</h5>
+            </label>
+            <label htmlFor="email" className="qa-question-modal-label">
+              email*:
+              <input type="text" id="email" placeholder="example@gmail.com" maxLength={60} />
+              <h5>For authentication reasons, you will not be emailed</h5>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
-        <div className="qa-question-modal-sub-header">
-          <h2 className="qa-question-modal-title">About the Product Name Here</h2>
-        </div>
-        {/* onSubmit={this.handleSubmit} */}
-        <form>
-          <label htmlFor="question">
-            Your Question*:
-            <textarea id="question" rows="4" cols="50" maxLength={1000} />
-            {/* <input type="text" id="question" /> */}
-          </label>
-          <label htmlFor="nickname">
-            Nickname*:
-            <input type="text" id="nickname" maxLength={60} />
-          </label>
-          <label htmlFor="email">
-            email*:
-            <input type="text" id="email" placeholder="Why did you like the product or not?" maxLength={60} />
-            <h5>For authentication reasons, you will not be emailed</h5>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default AddQuestionModal;
