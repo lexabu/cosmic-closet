a33377c3c6f678c1be186c5345e648ea758d0912
Josh Andromidas <git@joshandromidas.com>
2022-04-14 14:23:53 -0500

    remove unnecessary dependencies to reduce bundle size

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
 import axios from 'axios';
-import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
 import ProductDetail from '../containers/ProductDetail.jsx';
 import QuestionsAnswers from '../containers/QuestionsAnswers.jsx';
@@ -56,17 +54,6 @@ function App() {
         <ProductDetail />
         <QuestionsAnswers />
         <RatingsReviews />
-        <ToastContainer
-          position="top-center"
-          autoClose={4000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss={false}
-          draggable
-          pauseOnHover
-        />
       </main>
     </>
   );
