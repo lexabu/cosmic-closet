0111191d842e7de6b53dc3811dfd18df5e843f16
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-04-15 11:10:02 -0500

    add compression

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const expressStaticGzip = require('express-static-gzip');
+const compression = require('compression');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
+app.use(compression());
 app.use(expressStaticGzip(publicPath));
 
 // Making sure that all paths will serve up the same index.html file
