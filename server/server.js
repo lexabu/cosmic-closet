2c6b625620fe6c8dbb454f7db582a8988fd54d73
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-04-15 10:16:23 -0500

    add in express static gzip

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
+const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(expressStaticGzip(publicPath));
 
 // Making sure that all paths will serve up the same index.html file
 app.get('*', (req, res) => {
